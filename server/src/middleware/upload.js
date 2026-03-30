d01aa636efa74479a5de181a4c66efe2678c6c28
Egere Ofejiro <egereofejiro10@gmail.com>
2026-03-30 13:39:32 +0200

    fix: uploads

--- server/src/middleware/upload.js
+++ server/src/middleware/upload.js
@@ -1,10 +1,18 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+// ensure uploads folder exists
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 // storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + path.extname(file.originalname);
